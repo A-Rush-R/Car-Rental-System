c9aef373bd50bac853aabeac7b00e0fcbe23028a
Arush <arush.up@gmail.com>
2024-02-27 00:49:13 +0530

    Added the funcitonality for customer/employee to login

--- rough.cpp
+++ rough.cpp
@@ -1,41 +1,26 @@
-    // Function to add a new Employee to the database
-        static void addEmployee(vector<Employee>& Employees, const Employee& newEmployee) {
-            Employees.push_back(newEmployee);
-        }
+        static Customer* login(vector<Customer>& customers) {
+            string name, password;
+            cout << "Enter Username: ";
+            cin >> name;
+            cout << "Enter Password: ";
+            cin >> password;
 
-        // Function to update an existing Employee in the database
-        static void updateEmployee(vector<Employee>& Employees, int EmployeeId, const Employee& updatedEmployee) {
-            auto it = std::lower_bound(Employees.begin(), Employees.end(), EmployeeId, [](const Employee& Employee, int id) {
-                return Employee.id < id;
-            });
-            if (it != Employees.end()) {
-                *it = updatedEmployee;
-            } else {
-                cout << "Employee with ID " << EmployeeId << " not found." << endl;
-            }
-        }
-
-        // Function to delete a Employee from the database
-        static void deleteEmployee(vector<Employee>& Employees, int EmployeeId) {
-            auto it = std::lower_bound(Employees.begin(), Employees.end(), EmployeeId, [](const Employee& Employee, int id) {
-                return Employee.id < id;
-            });
-            if (it != Employees.end()) {
-                Employees.erase(it);
-            } else {
-                cout << "Employee with ID " << EmployeeId << " not found." << endl;
-            }
-        }
-
-        // Function to search for a Employee by ID
-        static Employee* searchEmployeeById(vector<Employee>& Employees, int EmployeeId) {
-            auto it = std::lower_bound(Employees.begin(), Employees.end(), EmployeeId, [](const Employee& Employee, int id) {
-                return Employee.id < id;
+            // Search for the customer with the given name
+            auto it = find_if(customers.begin(), customers.end(), [&name](const Customer& customer) {
+                return customer.name == name;
             });
 
-            if (it != Employees.end() && it->id == EmployeeId) {
-                return &(*it); 
+            // If customer found, check password
+            if (it != customers.end()) {
+                if (it->password == password) {
+                    cout << "Login Successful!" << endl;
+                    return &(*it);
+                } else {
+                    cout << "Incorrect Password!" << endl;
+                    return nullptr;
+                }
             } else {
-                return nullptr; 
+                cout << "Customer not found!" << endl;
+                return nullptr;
             }
         }
