35abf2317fd45de905abe8943db141e30d9fed14
Arush <arush.up@gmail.com>
2024-03-03 13:34:12 +0530

    Documented and edited the constants

--- constants.h
+++ constants.h
@@ -8,18 +8,18 @@
 #define CUSTOMER_BEGIN_ID 2 * NUM_USERS
 #define EMPLOYEE_BEGIN_ID 3 * NUM_USERS
 #define MANAGER_BEGIN_ID 4 * NUM_USERS
-#define REPAIR_REWARD 50
-#define NAME_CAP 20
+#define REPAIR_REWARD 50 //reward for repairing car given to employee (one unit per unit condition improved)
+#define NAME_CAP 20 //maximum length of name
 #define HEAVY_DAMAGE 1
 #define LIGHT_DAMAGE 2
 #define MINOR_SCRATCHES 3
 #define FINE 4 
 #define DISCOUNT 0.15 //discount for the employees (between 0 and 1)
-#define CUSTOMER_RECORD 200 //default value
-#define EMPLOYEE_RECORD 200 //default value
-#define RETURN_REWARD 200
-#define DAMAGE_PENALTY 50
-#define LATE_RECORD_CHARGE 20
-#define THRESHOLD_RECORD 200
+#define THRESHOLD_RECORD 200 // minimum record to be able to rent a car
+#define CUSTOMER_RECORD  THRESHOLD_RECORD * 2//default value
+#define EMPLOYEE_RECORD THRESHOLD_RECORD * 3 //default value
+#define RETURN_REWARD THRESHOLD_RECORD * 2 //reward for returning the car
+#define DAMAGE_PENALTY THRESHOLD_RECORD / 4 // penalty per unit change in condition of the car when returned (assumed the condition is at most same at the time of rental)
+#define LATE_RECORD_CHARGE THRESHOLD_RECORD / 4 //per day charge of returning the car late
 
 #endif 
